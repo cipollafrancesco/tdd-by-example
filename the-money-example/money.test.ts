6c5d8b3797556b4cee4e86cc621486dded3596cf
cipollafrancesco <francesco.cipolla@chili.com>
2022-09-19 10:26:11 +0200

    Refactored multiplication test case

--- the-money-example/money.test.ts
+++ the-money-example/money.test.ts
@@ -2,8 +2,8 @@ import Dollar from "./money";
 
 describe("First book chapter", function () {
     it("Should test the multiplication", function () {
-        const dollar = new Dollar(5).times(2);
-        expect(dollar.amount).toBe(10);
+        const five = new Dollar(5);
+        expect(new Dollar(10)).toEqual(five.times(2));
     })
 
     it("Should test the equality", function () {
